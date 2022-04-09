19f24dbd7143f1092081a52a73cd7c317c1b521e
Darkevil003 <allanfuentes1996@gmail.com>
2022-04-08 20:56:55 -0600

    Add project files.

---
+++ SUMA/SUMA/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SUMA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SUMA()
+            {
+
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SUMA/SUMA/SUMA.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SUMA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SUMA : ContentPage
+    {
+        public SUMA()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double r = Convert.ToDouble(numero1.Text);
+            double r1 = Convert.ToDouble(numero2.Text);
+            Double suma = (r + r1);
+
+            
+            result.Text = suma.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            numero1.Text = "";
+            numero2.Text = "";
+            
+            result.Text = "";
+
+        }
+    }
+}
